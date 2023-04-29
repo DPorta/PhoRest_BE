f73e32d2fd4d33c5b782a6b35471f763a05d8d8b
ChristopherMendozaIntelecta <127980632+ChristopherMendozaIntelecta@users.noreply.github.com>
2023-04-29 17:58:16 -0500

    added classfier h5

---
+++ classifier.py
@@ -0,0 +1,26 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+def clasiffier_CNN_predict(file):
+    class_dict = {'Imágen Borrosa': 0, 'Imágen Borrosa y Agrietada': 1, 'Imágen Agrietada': 2}
+    classes = []
+    for key in class_dict.keys():
+        classes.append(key)
+
+    model_h5_path = 'model_inception.h5'
+    model_new = load_model(model_h5_path, compile=False)
+
+    #img_ = load_img(file, target_size=(512, 512))
+    img_array = img_to_array(file)
+    img_processed = np.expand_dims(img_array, axis=0)
+    img_processed /= 255.
+
+    prediction = model_new.predict(img_processed)
+
+    index = np.argmax(prediction)
+
+    return str(classes[index])
--- main.py
+++ main.py
@@ -1,50 +1,35 @@
 from flask import Flask, request, jsonify
 from PIL import Image
+from classifier import clasiffier_CNN_predict
+import tensorflow as tf
 import io
 import base64
+import os
 
 app = Flask(__name__)
-
-# Definir una función para convertir la imagen a blanco y negro
-
-
-def convert_to_bw(image_file):
-    img = Image.open(image_file)
-    img = img.convert('L')
-    # Guardar la imagen en un buffer
-    buffer = io.BytesIO()
-    img.save(buffer, format='JPEG')
-    # Retornar la imagen procesada como string
-    bw_img_raw = buffer.getvalue()
-    bw_img_str = bw_img_raw.decode('latin-1')
-    return bw_img_str
-
-
-@app.route('/restoreImage', methods=['POST'])
-def convert_image():
-    image_file = request.files['image']
-    bw_image = convert_to_bw(image_file)
-    # print(bw_image_str)
-    return jsonify({'status': 'imagen recibida correctamente', 'processed': bw_image})
-
 
 @app.route('/getbase64', methods=['POST'])
 def show_base64():
     # Decodificar la cadena de base64 a una imagen
     base64img = request.form['imageb64']
-    # print("11111111111111 BASE 64 RECEIVED->", base64img)
+
     decoded_string = base64.b64decode(base64img)
-    # print("22222222222222 BASE 64 Decoded ->", decoded_string)
-    # Convertir la imagen a escala de grises
-    imageBW = Image.open(io.BytesIO(decoded_string)).convert('L')
-    # print("333333333333 Image BW CODE->", imageBW)
+
+
+
+    #Convertir imagen a PIL Object
+    imageBW = Image.open(io.BytesIO(decoded_string))
+    imageBWresized = imageBW.resize((512, 512))
+
+    #Enviar PIL Object al clasificador
+    result_casiffier = clasiffier_CNN_predict(imageBWresized)
+
     # Codificar la imagen en base64 nuevamente
     bufferedImg = io.BytesIO()
-    imageBW.save(bufferedImg, format="JPEG")
+    imageBWresized.save(bufferedImg, format="JPEG")
     base64BW = base64.b64encode(bufferedImg.getvalue()).decode('latin-1')
-    # print("4444444444444 imagep b64->", base64BW)
-    return jsonify({'status': 'base64 recibida correctamente', 'base64': base64img, 'base64P': base64BW})
-    # return jsonify({'status': 'base64 recibida correctamente', 'base64': base64img})
+
+    return jsonify({'status': 'base64 recibida correctamente', 'base64': base64img, 'base64P': base64BW, 'clase': result_casiffier})
 
 @app.route('/')
 def index():
