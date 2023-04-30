de2ff3a9cb74a7bd48e643cd79f0f16aebdc1de0
ChristopherMendozaIntelecta <127980632+ChristopherMendozaIntelecta@users.noreply.github.com>
2023-04-29 23:25:42 -0500

    gfp-gan added

---
+++ image_functions.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from io import BytesIO
+import base64
+import requests
+
+# Convert Image to Base64
+def im_2_b64(image):
+    buff = BytesIO()
+    image.save(buff, format="JPEG")
+    img_str = base64.b64encode(buff.getvalue()).decode('latin-1')
+    return img_str
+
+# Convert Base64 to Image
+def b64_2_img(data):
+    buff = BytesIO(base64.b64decode(data))
+    return Image.open(buff)
+
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content).decode('latin-1')
--- main.py
+++ main.py
@@ -1,35 +1,59 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, flash, render_template
 from PIL import Image
 from classifier import clasiffier_CNN_predict
+from gfp_gan_api import predict_gfpgan_image
+from image_functions import b64_2_img, im_2_b64, get_as_base64
 import tensorflow as tf
+import codecs
 import io
 import base64
 import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+################################################################
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
-@app.route('/getbase64', methods=['POST'])
-def show_base64():
-    # Decodificar la cadena de base64 a una imagen
-    base64img = request.form['imageb64']
+@app.route('/getfilename', methods=['POST'])
+def show_file_name():
+    # get image filename
+    get_file_img = request.files['filename']
 
-    decoded_string = base64.b64decode(base64img)
+    file_name = secure_filename(get_file_img.filename)
+    #print(get_file_img.filename)
+
+    full_file_name = "." + url_for("static", filename="images/" + file_name)
+    get_file_img.save(full_file_name)
 
 
+    #get_file_img2 = codecs.EncodedFile(request.files['filename'], "utf-8")
+    #bytes_obj = get_file_img2.read().encode('utf-8')
 
-    #Convertir imagen a PIL Object
-    imageBW = Image.open(io.BytesIO(decoded_string))
-    imageBWresized = imageBW.resize((512, 512))
+    #binary_file_data = get_file_img.read().decode("latin-1")
+
+    #open file as a PIL object and resize
+    imgPILresized = Image.open(get_file_img).resize((512, 512))
+
+    #Convert Image to b64
+    img_b64 = im_2_b64(imgPILresized)
 
     #Enviar PIL Object al clasificador
-    result_casiffier = clasiffier_CNN_predict(imageBWresized)
+    result_casiffier = clasiffier_CNN_predict(imgPILresized)
 
-    # Codificar la imagen en base64 nuevamente
-    bufferedImg = io.BytesIO()
-    imageBWresized.save(bufferedImg, format="JPEG")
-    base64BW = base64.b64encode(bufferedImg.getvalue()).decode('latin-1')
+    predicted_gfpgan_image_url = predict_gfpgan_image(full_file_name)
+    #print(predicted_gfpgan_image_url)
+    base64_predicted = get_as_base64(predicted_gfpgan_image_url)
 
-    return jsonify({'status': 'base64 recibida correctamente', 'base64': base64img, 'base64P': base64BW, 'imageResult': result_casiffier})
+
+    return jsonify({'status': 'base64 recibida correctamente', 'base64': img_b64, 'imageResult': result_casiffier, 'GFP-GAN': base64_predicted})
+
+
+
+###########################################
 
 @app.route('/')
 def index():
